7c8c4bdae7a96a53e4cae757f0b8a327319efa6a
j-qinche <j-qinche@microsft.com>
2018-11-30 17:12:12 +0800

    new

--- database.php
+++ database.php
@@ -1,11 +1,11 @@
 <?php
-$con = mysql_connect("localhost","root","123456");
+$con = mysqli_connect("localhost","root","123456");
 if (!$con){
   die('Could not connect: ' . mysql_error());
 }
-mysql_select_db("test", $con);
-$result = mysqli_connect("SELECT * FROM person");
-while($row=mysql_fetch_array($result)){
+mysqli_select_db("test", $con);
+$result = mysqli_query("SELECT * FROM person");
+while($row=mysqli_fetch_array($result)){
   echo $row['firstname'].' '.$row['lastname'];
   echo "<br />";
 }
