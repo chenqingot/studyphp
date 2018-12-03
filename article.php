a1575abf1c5804caa5536b5f98aaeafd43fc3f96
j-qinche <j-qinche@microsft.com>
2018-12-03 11:41:53 +0800

    n

--- article.php
+++ article.php
@@ -1,3 +1,21 @@
 姓 <?php echo $_POST["firstname"]; ?><br>
 名 <?php echo $_POST["lastname"]; ?>
 
+<?php
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "123456";
+$dbname = "test";
+
+$con = mysql_connect($servername, $username, $password);
+if (!$con){
+  die('Could not connect: ' . mysql_error());
+}
+mysql_select_db($dbname, $con);
+$result = mysql_query("SELECT * FROM person");
+while($row=mysql_fetch_array($result)){
+  echo $row['firstname'].' '.$row['lastname'];
+  echo "<br />";
+}
+?>
