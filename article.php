662e02e409a72a9fce1e74cc53f5cef79aad1a7f
j-qinche <j-qinche@microsft.com>
2018-12-03 12:08:19 +0800

    n

--- article.php
+++ article.php
@@ -7,12 +7,15 @@ $servername = "127.0.0.1";
 $username = "root";
 $password = "123456";
 $dbname = "test";
-
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$sql = "INSERT INTO person(firstname,lastname) values('".$firstname."','".$lastname."')";
 $con = mysql_connect($servername, $username, $password);
 if (!$con){
   die('Could not connect: ' . mysql_error());
 }
 mysql_select_db($dbname, $con);
+mysql_query("$sql");
 $result = mysql_query("SELECT * FROM person");
 while($row=mysql_fetch_array($result)){
   echo $row['firstname'].' '.$row['lastname'];
