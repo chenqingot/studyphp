1f1f783e75cd9a2fdb77bc2b2eb689db198c5c25
j-qinche <j-qinche@microsft.com>
2018-12-03 09:36:01 +0800

    n

--- article.php
+++ article.php
@@ -1,2 +1,21 @@
-欢迎 <?php echo $_POST["fname"]; ?>!<br>
-你的年龄是 <?php echo $_POST["age"]; ?>  岁。
+姓 <?php echo $_POST["firstname"]; ?><br>
+名 <?php echo $_POST["lastname"]; ?>
+
+<?php
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "123456";
+$dbname = "test";
+
+$con = mysql_connect($servername, $username, $password);
+if (!$con){
+  die('Could not connect: ' . mysql_error());
+}
+mysql_select_db($dbname, $con);
+$result = mysql_query("SELECT * FROM person");
+while($row=mysql_fetch_array($result)){
+  echo $row['firstname'].' '.$row['lastname'];
+  echo "<br />";
+}
+?>
