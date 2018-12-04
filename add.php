b6268c4fa6a4e62686466e4f66daded11057c6ff
j-qinche <j-qinche@microsft.com>
2018-12-04 17:42:39 +0800

    n

--- add.php
+++ add.php
@@ -4,18 +4,20 @@ $servername = "127.0.0.1";
 $username = "root";
 $password = "123456";
 $dbname = "test";
-$firstname = $_POST["firstname"];
-$lastname = $_POST["lastname"];
-$sql = "INSERT INTO person(firstname,lastname) values('".$firstname."','".$lastname."')";
+$title = $_POST["title"];
+$content = $_POST["content"];
+$category = $_POST["category"];
+
+$sql = "INSERT INTO article(title,content,category) values('".$title."','".$content."','".$category."')";
 $con = mysql_connect($servername, $username, $password);
 if (!$con){
   die('Could not connect: ' . mysql_error());
 }
 mysql_select_db($dbname, $con);
 mysql_query($sql);
-$result = mysql_query("SELECT * FROM person");
+$result = mysql_query("SELECT * FROM article");
 while($row=mysql_fetch_array($result)){
-  echo $row['firstname'].' '.$row['lastname'];
+  echo $row['title'].' '.$row['content'].' '.$row['category'];
   echo "<br />";
 }
 ?>
