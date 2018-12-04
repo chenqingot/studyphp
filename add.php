59aac0c621d71add0483d189db3f1faf556ebc7e
j-qinche <j-qinche@microsft.com>
2018-12-05 00:10:50 +0800

    n

--- add.php
+++ add.php
@@ -4,20 +4,22 @@ $servername = "127.0.0.1";
 $username = "root";
 $password = "123456";
 $dbname = "test";
-$title = $_POST["title"];
-$content = $_POST["content"];
-$category = $_POST["category"];
+// $title = $_POST["title"];
+// $content = $_POST["content"];
+// $category = $_POST["category"];
+$array = array();
 
-$sql = "INSERT INTO article(title,content,category) values('".$title."','".$content."','".$category."')";
+// $sql = "INSERT INTO article(title,content,category) values('".$title."','".$content."','".$category."')";
 $con = mysql_connect($servername, $username, $password);
 if (!$con){
   die('Could not connect: ' . mysql_error());
 }
 mysql_select_db($dbname, $con);
-mysql_query($sql);
+// mysql_query($sql);
 $result = mysql_query("SELECT * FROM article");
+
 while($row=mysql_fetch_array($result)){
-  echo $row['id'].' '.$row['title'].' '.$row['content'].' '.$row['category'];
-  echo "<br />";
+  $array[] = $row;
+  echo json_encode($array, JSON_FORCE_OBJECT);
 }
 ?>
