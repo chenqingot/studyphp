212f69f972ed09415f6585b02f55d9082e8bcd71
Qing Chen <j-qinche@microsoft.com>
2018-12-19 01:27:31 +0800

    n

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>钱币收藏</title>
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="shortcut icon" href="image/logo.ico">
+</head>
+<body>
+<div class="header">
+    <img src="image/logo.jpg" class="logo">
+    <h3 class="site-name">收藏家</h3>
+</div>
+<div class="nav">
+    <div class="nav-content">
+        <div class="category">最新资讯</div>
+        <div class="category">发行预告</div>
+        <div class="category">纪念钞</div>
+        <div class="category">纪念币</div>
+        <div class="category">第四套人民币收藏百科</div>
+        <div class="category">第三套人民币收藏百科</div>
+    </div>
+</div>
+<div class="con">
+    <div class="zxzx content">
+        <div class="title">
+            最新资讯
+        </div>
+        <div class="list">
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+        </div>
+    </div>
+    <div class="fxyg content">
+        <div class="title">
+            发行预告
+        </div>
+        <div class="list">
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+        </div>
+    </div>
+</div>
+<div class="con">
+    <div class="jnc content">
+        <div class="title">
+            纪念钞
+        </div>
+        <div class="list">
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+        </div>
+    </div>
+    <div class="jnb content">
+        <div class="title">
+            纪念币
+        </div>
+        <div class="list">
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+        </div>
+    </div>
+</div>
+<div class="con">
+    <div class="dst content" style="margin-right: 10px;">
+        <div class="title">
+            第四套人民币收藏百
+        </div>
+        <div class="list">
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+        </div>
+    </div>
+    <div class="dst content">
+        <div class="title">
+            第三套人民币收藏百
+        </div>
+        <div class="list">
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+            <p>AAAAAAAAAAAAAA</p>
+        </div>
+    </div>
+</div>
+
+<div class="footer">
+    <p>关于我们 | 联系我们</p>
+    <p>Copyright@1999-2012 99yq.com All Rights Reserved</p>
+</div>
+</body>
+</html>
